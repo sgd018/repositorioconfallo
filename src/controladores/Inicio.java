1ff4e1ce18503528b7ad63ee1507b6b6cce05ff2
sgd018 <samuel@sgd018.com>
2024-12-18 11:48:20 +0100

    Version 1

---
+++ src/controladores/Inicio.java
@@ -0,0 +1,11 @@
+package controladores;
+
+public class Inicio {
+	
+	public static void main(String[] args) {
+
+        System.out.println("¡Esto es una prueba para romper github!");
+
+    }
+	
+}
